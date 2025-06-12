3a298861adc1e38f4a649642ae7f5a4e577806f7
GmailHelene <65661117+GmailHelene@users.noreply.github.com>
2025-06-12 11:35:35 +0200

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,1 +1,22 @@
+const CACHE_NAME = "aksjeradar-cache-v1";
+const urlsToCache = [
+  "/",
+  "/static/css/style.css",
+  "/static/js/main.js",
+  "/static/images/logo.png"
+  // legg til flere statiske filer ved behov
+];
 
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
